634b3166b011f27da66c09d35c07b2fd0b3bfa73
Shashank7016 <bryanhovak@gmail.com>
2024-06-20 07:59:51 +1000

    improved user interaction and design for login and registration pages

--- login.php
+++ login.php
@@ -6,6 +6,8 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+    <div class="container">
+        <div class="form-container">
     <?php
     //The main purpose of this file is to allow user to login if registration is successful
     session_start();
@@ -18,41 +20,56 @@
     
     if (isset($_GET['register'])) {
         echo '
+        <h2>Register</h2>
         <form action="register.php" method="post">
-            <label for="firstname">First Name:</label>
-            <input type="text" id="firstname" name="firstname" required>
-            <br>
-            <label for="surname">Surname:</label>
-            <input type="text" id="surname" name="surname" required>
-            <br>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            <br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-            <br>
-            <label for="confirm_password">Confirm Password:</label>
-            <input type="password" id="confirm_password" name="confirm_password" required>
-            <br>
-            <input type="submit" name="register" value="Register">
-        </form>';
+            <div class="form-group">
+                        <label for="firstname">First Name:</label>
+                        <input type="text" id="firstname" name="firstname" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="surname">Surname:</label>
+                        <input type="text" id="surname" name="surname" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="confirm_password">Confirm Password:</label>
+                        <input type="password" id="confirm_password" name="confirm_password" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="register" value="Register" class="btn">
+                    </div>
+                </form>';
     } else {
         ?>
+        <h2>Login</h2>
         <form action="register.php" method="post">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            <br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-            <br>
-            <input type="submit" name="login" value="Login">
-        </form>
-
-        <br>
-        <a href="login.php?register=1">Register as a new user</a>
+        <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="login" value="Login" class="btn">
+                    </div>
+                </form>
+                <div class="register-link">
+                    <a href="login.php?register=1">Register as a new user</a>
+                </div>
         <?php
     }
     ?>
+     </div>
+     </div>
     <script src="js/scripts.js"></script>
 </body>
 </html>
